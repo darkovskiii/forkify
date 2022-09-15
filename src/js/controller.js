e2cba055f4911a47268f57dcb6196a27809a4dae
darkovskiii <darkotodorovski93@gmail.com>
2022-09-15 15:10:46 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -113,6 +113,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to app');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -121,6 +125,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
